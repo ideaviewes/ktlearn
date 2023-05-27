6f5cf6e2b8f41c60b515fa22d178c50c81defdfb
icodeview <fzypzl@outlook.com>
2023-05-27 15:18:41 +0800

    test

---
+++ src/test/kotlin/com/icodeview/kt/KtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.icodeview.kt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
